212313ebf2df24fcd26304e8a4b086cabe803a47
Unknown <01.lexandre@gmail.com>
2017-04-26 22:05:30 -0300

    Navbar SideBar

---
+++ src/components/partials/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sidebar" data-color="purple" data-image="static/img/sidebar-4.jpg">
+    <div class="logo">
+      <a href="http://www.creative-tim.com" class="simple-text">
+        Creative Tim
+      </a>
+    </div>
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="active">
+          <a href="#">
+            <i class="material-icons">dashboard</i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="active-pro">
+          <a>
+            <i class="material-icons">unarchive</i>
+            <p>01lexandre</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
--- src/domains/home/components/Home.vue
+++ src/domains/home/components/Home.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="container ">
+  <div class="">
     <h1>{{ msg }}</h1>
-    <button class="btn btn-primary">Default<div class="ripple-container"></div></button>
-    <div class="form-group label-floating is-empty">
-      <label class="control-label">With Floating Label</label>
-      <input type="email" class="form-control">
-      <span class="material-input"></span></div>
   </div>
 </template>
 
